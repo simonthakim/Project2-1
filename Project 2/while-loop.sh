825ef4de1ee55003eebcf9e37c84664621b8cd26
simonthakim <71240865+simonthakim@users.noreply.github.com>
2020-11-03 16:19:47 -0600

    Update while-loop.sh

--- Project 2/while-loop.sh
+++ Project 2/while-loop.sh
@@ -1,14 +1,15 @@
+//This block executes until the count equals to 5
 #!/bin/bash
 
 valid=true
 count=1
 while [ $valid ]
 do
-echo $count
-if [ $count -eq 5 ];
+echo $count // prints the value of "count" 
+if [ $count -eq 5 ];/ it checks wether count equal to 5 or not if it is equal them breaks the loop and comes out of block else increaments the value of count and continues the loop
 then
 break
 fi
-((count++))
+((count++)) //it is interesting that he used a c++ approach
 done
 
