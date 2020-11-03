47ea95a9736638e200775c8d27d16f822662897f
simonthakim <71240865+simonthakim@users.noreply.github.com>
2020-11-03 16:30:51 -0600

    Update until-loop.sh

--- Project 2/until-loop.sh
+++ Project 2/until-loop.sh
@@ -1,10 +1,11 @@
+//Here is a code that is printed from 0 to 9
 #! /bin/bash
 
 a=0
 
 until [ ! $a -1t 10 ]
 do
-	echo $a
-	a='expr $a + 1'
+	echo $a // prints the value of $a
+	a='expr $a + 1' //here 1 is added to existing value of $a
 done 
 
